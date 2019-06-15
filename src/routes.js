9bee5977ad1e2fa8d18452b4d3351246f800c4b2
Dhemes Mota <dhemes.mota@gmail.com>
2019-06-15 15:42:51 -0300

    Primeiro commit

---
+++ src/routes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const multer = require('multer');
+const uploadConfig = require('./config/upload');
+const PostController = require('./controllers/PostController');
+const LikeController = require('./controllers/LikeController');
+
+const routes = new express.Router();
+const upload = multer(uploadConfig);
+
+// No express tem middle, que é uma função que sempre recebe (req, res) e retona alguma coisa
+routes.get('/posts', PostController.index);
+routes.post('/posts', upload.single('image'), PostController.store);
+
+routes.post('/posts/:id/like', LikeController.store);
+
+module.exports = routes;
